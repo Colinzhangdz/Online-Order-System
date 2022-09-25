fe5ea75f37db0eacc0642e033e3ad0faa3b847ed
ivy-zyx01 <yingxi.zeng-1@student.uts.edu.au>
2022-09-25 16:42:11 +0800

    Add data model for customer and staff

---
+++ src/main/java/com/group7/asd/model/Staff.java
@@ -0,0 +1,67 @@
+package com.group7.asd.model;
+
+public class Staff {
+    private String id;
+    private String name;
+    private String gender;
+    private String age;
+    private String phone;
+    private String username;
+    private String password;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
