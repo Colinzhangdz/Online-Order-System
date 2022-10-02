9f6290696aa86e935b10fb3d57ebebf346f7f1c5
Colinzhangdz <dingzhao.zhang@student.uts.edu.au>
2022-10-02 21:13:53 +0800

    Add EvaluationTest

---
+++ src/test/java/EvaluationTest.java
@@ -0,0 +1,40 @@
+import com.group7.asd.dao.EvaluationDao;
+import com.group7.asd.model.Evaluation;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+public class EvaluationTest {
+
+    //Test add evaluation
+    @Test
+    public void test1() throws Exception{
+        Evaluation evaluation = new Evaluation();
+        evaluation.setId("a6e06601-5953-4bfb-ad81-f61917f19558");
+        evaluation.setPlatformRating(7);
+        evaluation.setFeedback("Test");
+        evaluation.setDeliveryPersonFeedback("Excellent");
+        evaluation.setAttraction(Arrays.asList(new String[]{"Recommended by others","Attractive advertising"}));
+        evaluation.setFoodOverallRating(3);
+        evaluation.setFoodPackingRating(4);
+        evaluation.setFoodTasteRating(5);
+        EvaluationDao evaluationDao = new EvaluationDao();
+        evaluationDao.createEvaluation(evaluation);
+
+        Evaluation evaluationTest = evaluationDao.getEvaluationById("a6e06601-5953-4bfb-ad81-f61917f19558");
+        assertEquals(7, evaluationTest.getPlatformRating());
+    }
+
+    //Test delete evaluation
+    @Test
+    public void test2() throws Exception{
+        EvaluationDao evaluationDao = new EvaluationDao();
+        evaluationDao.deleteEvaluationById("a6e06601-5953-4bfb-ad81-f61917f19558");
+
+        Evaluation evaluationTest = evaluationDao.getEvaluationById("a6e06601-5953-4bfb-ad81-f61917f19558");
+        assertNull(evaluationTest.getId());
+    }
+}
