d6abd4cf3ca7d9118e9bce08dfbe09d483bccea7
Zhaoyu Pan <panzhaoyu11@gmail.com>
2022-10-03 16:45:29 +1100

    add User function Junit test

---
+++ src/test/java/RegisterTest.java
@@ -0,0 +1,40 @@
+import com.group7.asd.dao.UserDBManager;
+import com.group7.asd.model.User;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+public class RegisterTest {
+
+    //Test add user
+    @Test
+    public void testUser1() throws Exception{
+        User user = new User();
+        user.setUserId(6666);
+        user.setEmail(666@uts.com);
+        user.setPassword(1234);
+        user.setFullName(Zhoayu Pan);
+        user.setPhone(1234567890);
+        user.setUserType(staff);
+        user.setIsActive(true);
+
+        UserDBManager userDBManager = new UserDBManager();
+        userDBManager.addUser(user);
+
+        User userTest = UserDBManager.findUser("Zhaoyu Pan", "1234");
+        assertEquals(1234567890, userTest.getPhone);
+    }
+
+    //Test delete user
+    @Test
+    public void testUser2() throws Exception{
+        UserDBManager userDBManager = new UserDBManager();
+        userDBManager.deleteUser(6666);
+
+        User userTest = userDBManager.findUser("Zhoayu Pan", "1234");
+        assertNull(userTest.getUserId());
+    }
+}
