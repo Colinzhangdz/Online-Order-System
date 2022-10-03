c3af0160ab938918f649f91b5c827e3fcada5174
ivy-zyx01 <yingxi.zeng-1@student.uts.edu.au>
2022-10-03 20:01:02 +0800

    Add junit test for staff and add page links

---
+++ src/test/asd/StaffTest.java
@@ -0,0 +1,11 @@
+package com.group7.asd;
+
+import org.junit.jupiter.api.*;
+
+public class StaffTest {
+    @Test
+    public void testStaff(){
+        Assertions.assertEquals(1,2);
+    }
+
+}
---
+++ src/test/java/StaffTest.java
@@ -0,0 +1,23 @@
+import com.group7.asd.dao.Database;
+import com.group7.asd.model.Staff;
+import org.junit.jupiter.api.*;
+
+import java.util.ArrayList;
+
+public class StaffTest {
+    @Test
+    public void testAddStaff() {
+        ArrayList<Staff> staffs = Database.instance().getStaffs();
+        Staff staff = new Staff();
+        staff.setAge("30");
+        staff.setGender("male");
+        staff.setName("Test Name");
+        staff.setPassword("1234");
+        staff.setPhone("11111111111");
+        Database.instance().addStaff(staff);
+        ArrayList<Staff> staffs2 = Database.instance().getStaffs();
+        Assertions.assertEquals(1, staffs2.size() - staffs.size());
+
+    }
+
+}
